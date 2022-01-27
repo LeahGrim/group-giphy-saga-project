2bb1b998b5bcfb885254ed5308bbe8c97ca8bc7f
Jacob Larson <jacob.larson55112@gmail.com>
2022-01-27 13:33:06 -0600

    set up SAGAs to local DB, made fav component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import { render } from "react-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+//components
+import Favorite from '../Favorite/Favorite'
 
-function App(props) {
+function App() {
   return (
     <div>
       <h1>Giphy Search!</h1>
+
+      <BrowserRouter>
+      <Route path="/" exact>
+        <Favorite />
+      </Route>
+  </BrowserRouter>,
+
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -14,9 +14,21 @@ import axios from 'axios';
 
 // Create the rootSaga/watcherSaga generator function
 function* rootSaga() {
-
+yield takeEvery('FETCH_FAVORITES', fetchFavs)
 
 } // end function rootSaga
+function* fetchFavs(){
+    console.log('in saga');
+    try{
+        let response = yield axios.get('/api/favorite');
+        yield put({
+            type: 'SET_FAVORITES',
+            payload: response.data
+        })
+    } catch(err){
+        console.error(err);
+    }
+}
 
 
 // Create sagaMiddleware
