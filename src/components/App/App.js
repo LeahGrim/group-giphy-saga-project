60b95a56683d4b28630db344c826fd9233ba411d
Leah Grim <grimchelate@gmail.com>
2022-01-27 13:50:21 -0600

    styling changes on input form

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import GiphyForm from '../GiphyForm/GiphyForm.jsx'
 import './App.css'
 import Header from '../Header/Header.jsx'
+import Footer from '../Footer/Footer.jsx'
 function App(props) {
   return (
-    <div>
+    <div className= "App">
       <Header />
      
       <GiphyForm />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import './Footer.css'
+function Footer(){    
+    return(
+    <>
+    <div className="footerTitle">
+    <footer>
+    <h1> Thanks For Getting Giphy With Us</h1>
+    </footer>
+    </div>
+    </>
+    )
+}
+export default Footer;
