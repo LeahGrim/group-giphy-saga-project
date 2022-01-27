bc5f474965d6b7a1b4425b10f49794239fafe9be
Chris Agbolade <yemluck@gmail.com>
2022-01-27 11:26:18 -0600

    modifications to result component

--- src/components/Results/Results.js
+++ src/components/Results/Results.js
@@ -8,19 +8,24 @@ function Results () {
 
     const selectedGif = useSelector(store => store.resultsList)
 
-    const likePix = () => {
+    const likePix = (liked) => {
         console.log('in likePix');
         dispatch ({
-            type: 'IMAGE_LIKED'
-        // payload: ....
+            type: 'IMAGE_LIKED',
+            payload: liked
         })
         
     }
 
+    // note .... saga should listen for a dispatch message of "IMAGE_LIKED" 
+    // containing a payload of the particular image liked
+    // when it receives this, it should send it to a store 
+    // which will be a list of all images liked
+
     return (
         <>
         <ul>
-            {selectedGif.map((eachGif, index) => (<li key={index}> {<img src={eachGif} alt='cool image' width={200} />} <button onClick={likePix}> ❤️ </button></li>))}
+            {selectedGif.map((eachGif, index) => (<li key={index}> {<img src={eachGif} alt='cool image' width={200} />} <button onClick={() => likePix(liked)}> ❤️ </button></li>))}
         </ul>
         
         
